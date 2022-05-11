8aa37b105c7f84f9cc66d7517e1ee69791b15b66
erikamsk <65354161+erikamsk@users.noreply.github.com>
2022-05-11 20:27:37 +0100

    test

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const burger = document.getElementById('burger');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefaultprevent()
+    const container = document.getElementById('container');
+    container.classList.toggle('isactive');
+
+    const isactive = container.classList.contains('isactive');
+    event.currentTarget.setAttribute('aria-espanded', 'true');
+
+    if (isactive) {
+        event.currentTarget.setAttribute('aria-label', 'close menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'open menu');
+    }
+}
+
+burger.addEventListener('click', toggleMenu);
+burger.addEventListener('touchstart', toggleMenu);
